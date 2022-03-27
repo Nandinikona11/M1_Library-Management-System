a650a63469c43e57f2e461846068d75ca558149a
Nandinikona11 <101653632+Nandinikona11@users.noreply.github.com>
2022-03-27 12:38:05 +0530

    Update addbooks.c

--- 3_Implementation/src/addbooks.c
+++ 3_Implementation/src/addbooks.c
@@ -1,47 +1,70 @@
-void addbooks(void)    //funtion that add books
+void addBookInDataBase()
 {
-system("cls");
-int i;
-gotoxy(20,5);
-printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2SELECT CATEGOIES\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-gotoxy(20,7);
-printf("\xDB\xDB\xDB\xDB\xB2 1. Computer");
-gotoxy(20,9);
-printf("\xDB\xDB\xDB\xDB\xB2 2. Electronics");
-gotoxy(20,11);
-printf("\xDB\xDB\xDB\xDB\xB2 3. Electrical");
-gotoxy(20,13);
-printf("\xDB\xDB\xDB\xDB\xB2 4. Civil");
-gotoxy(20,15);
-printf("\xDB\xDB\xDB\xDB\xB2 5. Mechanical");
-gotoxy(20,17);
-printf("\xDB\xDB\xDB\xDB\xB2 6. Architecture");
-gotoxy(20,19);
-printf("\xDB\xDB\xDB\xDB\xB2 7. Back to main menu");
-gotoxy(20,21);
-printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-gotoxy(20,22);
-printf("Enter your choice:");
-scanf("%d",&s);
-if(s==7)
-
-mainmenu() ;
-system("cls");
-fp=fopen("Bibek.dat","ab+");
-if(getdata()==1)
-{
-a.cat=catagories[s-1];
-fseek(fp,0,SEEK_END);
-fwrite(&a,sizeof(a),1,fp);
-fclose(fp);
-gotoxy(21,14);
-printf("The record is sucessfully saved");
-gotoxy(21,15);
-printf("Save any more?(Y / N):");
-if(getch()=='n')
-mainmenu();
-else
-system("cls");
-addbooks();
-}
+    int days;
+    s_BooksInfo addBookInfoInDataBase = {0};
+    FILE *fp = NULL;
+    int status = 0;
+    fp = fopen(FILE_NAME,"ab+");
+    if(fp == NULL)
+    {
+        printf("File is not opened\n");
+        exit(1);
+    }
+    headMessage("ADD NEW BOOKS");
+    printf("\n\n\t\t\tENTER YOUR DETAILS BELOW:");
+    printf("\n\t\t\t---------------------------------------------------------------------------\n");
+    printf("\n\t\t\tBook ID NO  = ");
+    fflush(stdin);
+    scanf("%u",&addBookInfoInDataBase.books_id);
+    do
+    {
+        printf("\n\t\t\tBook Name  = ");
+        fflush(stdin);
+        fgets(addBookInfoInDataBase.bookName,MAX_BOOK_NAME,stdin);
+        status = isNameValid(addBookInfoInDataBase.bookName);
+        if (!status)
+        {
+            printf("\n\t\t\tName contain invalid character. Please enter again.");
+        }
+    }
+    while(!status);
+    do
+    {
+        printf("\n\t\t\tAuthor Name  = ");
+        fflush(stdin);
+        fgets(addBookInfoInDataBase.authorName,MAX_AUTHOR_NAME,stdin);
+        status = isNameValid(addBookInfoInDataBase.authorName);
+        if (!status)
+        {
+            printf("\n\t\t\tName contain invalid character. Please enter again.");
+        }
+    }
+    while(!status);
+    do
+    {
+        printf("\n\t\t\tStudent Name  = ");
+        fflush(stdin);
+        fgets(addBookInfoInDataBase.studentName,MAX_STUDENT_NAME,stdin);
+        status = isNameValid(addBookInfoInDataBase.studentName);
+        if (!status)
+        {
+            printf("\n\t\t\tName contain invalid character. Please enter again.");
+        }
+    }
+    while(!status);
+    do
+    {
+        //get date year,month and day from user
+        printf("\n\t\t\tEnter date in format (day/month/year): ");
+        scanf("%d/%d/%d",&addBookInfoInDataBase.bookIssueDate.dd,&addBookInfoInDataBase.bookIssueDate.mm,&addBookInfoInDataBase.bookIssueDate.yyyy);
+        //check date validity
+        status = isValidDate(&addBookInfoInDataBase.bookIssueDate);
+        if (!status)
+        {
+            printf("\n\t\t\tPlease enter a valid date.\n");
+        }
+    }
+    while(!status);
+    fwrite(&addBookInfoInDataBase,sizeof(addBookInfoInDataBase), 1, fp);
+    fclose(fp);
 }
