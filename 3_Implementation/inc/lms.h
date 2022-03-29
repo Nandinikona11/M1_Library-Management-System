5e250f8f85394480f310a33f3b2f42ff0a7a3b26
Nandinikona11 <101653632+Nandinikona11@users.noreply.github.com>
2022-03-29 12:46:57 +0530

    Update lms.h

--- 3_Implementation/inc/lms.h
+++ 3_Implementation/inc/lms.h
@@ -6,71 +6,142 @@
 #include<string.h>
 #include<conio.h>
 #include<windows.h>
+#define MAX_YR  9999
+#define MIN_YR  1900
+#define MAX_SIZE_USER_NAME 30
+#define MAX_SIZE_PASSWORD  20
+#define FILE_NAME  "abc.c"
+// Macro related to the books info
+#define MAX_BOOK_NAME   50
+#define MAX_AUTHOR_NAME 50
+#define MAX_STUDENT_NAME 50
+#define MAX_STUDENT_ADDRESS 300
+#define FILE_HEADER_SIZE  sizeof(sFileHeader)
 
-
-/**
- * @brief structure for Book Record
- * 
- */
-typedef struct books
+typedef struct
 {
-    int book_id;
-    char book_name[20];
-    char author_name[20];
-}books;
+    int yyyy;
+    int mm;
+    int dd;
+} Date;
+typedef struct
+{
+    char username[MAX_SIZE_USER_NAME];
+    char password[MAX_SIZE_PASSWORD];
+} sFileHeader;
+typedef struct// to call in program
+{
+    unsigned int books_id; // declare the integer data type
+    char bookName[MAX_BOOK_NAME];// declare the character data type
+    char authorName[MAX_AUTHOR_NAME];// declare the charecter data type
+    char studentName[MAX_STUDENT_NAME];// declare the character data type
+    char studentAddr[MAX_STUDENT_ADDRESS];// declare the character data type
+    Date bookIssueDate;// declare the integer data type
+} s_BooksInfo;
 
 /**
  * @brief return type for funtions for unit testing
  * 
  */
 
+typedef enum test_values {
+    pass = 1,
+    fail = 0
+}test_values;
 
- void mainmenu()
+/**
+ * @brief function to find a book by its ID
+ * @param id 
+ * @return test_values 
+ */
+
+int isNameValid(const char *name);
+
+/**
+  * @brief decription about names
+  *
+  * @return validname
+  */
+int  IsLeapYear(int year);
+
+/**
+ * @brief description about year
+ *
+ * @return 1 if year is valid
+ */
+
+int isValidDate(Date *validDate);
+/**
+ * @brief decription about date
+ *
+ * @return 1 if date is valid
+ */
+
+ test_values menu()
  
  /**
   * @brief description about mainmenu
   *
-  * @return void
-  * /
+  * @return test_values
+  */
   
-void searchbook(int id);
+test_values searchbook(int id);
 
 /**
  * @brief function to add new books to the library
  * 
- * @return values 
+ * @return test_values 
  */
-void addbook(int id,char name[],char author[])
+test_values addbook();
 
 /**
- * @brief funtion to denote discarded books
+ * @brief funtion to add books
  * 
  * @param id 
- * @return values 
+ * @return test_values 
  */
-void deletebook(int id);
+test_values deletebooks(int id);
 
 /**
  * @brief function to view all the books
  * 
  * @return test_values 
  */
-void issuebook(int id);
+test_values updatecredential(void);
 
 /**
- * @brief function to issue the books
+ * @brief function to update the credentials
  * 
  * @return test_values 
  */
- void editbook(int id);
-
-/**
- * @brief function to edit the books
- * 
- * @return values 
- */
  
-void viewbook(void);
+test_values login();
+/**
+ * @brief decription about login access
+ *
+ * @return test_values
+ */
+
+int isFileExists();
+/**
+ * @brief decription to check if file exists
+ *
+ * @return 1 if file exists
+ */
+
+void init();
+/**
+ * @brief description 
+ *
+ * @return 1
+ */
+
+int main();
+/**
+ * //main function
+ */
+
+test_values viewbooks(void);
 
 #endif
 
