fc80993d8e1d8496c8dd9f774a7c1662ddf04814
Nandinikona11 <101653632+Nandinikona11@users.noreply.github.com>
2022-03-29 13:21:12 +0530

    Create Init.c

---
+++ 3_Implementation/src/Init.c
@@ -0,0 +1,23 @@
+#include<lms.h>
+void init()
+{
+    FILE *fp = NULL;
+    int status = 0;
+    const char defaultUsername[] ="username\n";
+    const char defaultPassword[] ="password\n";
+    sFileHeader fileHeaderInfo = {0};
+    status = isFileExists(FILE_NAME);
+    if(!status)
+    {
+        //create the binary file
+        fp = fopen(FILE_NAME,"wb");
+        if(fp != NULL)
+        {
+            //Copy default password
+            strncpy(fileHeaderInfo.password,defaultPassword,sizeof(defaultPassword));
+            strncpy(fileHeaderInfo.username,defaultUsername,sizeof(defaultUsername));
+            fwrite(&fileHeaderInfo,FILE_HEADER_SIZE, 1, fp);
+            fclose(fp);
+        }
+    }
+}
