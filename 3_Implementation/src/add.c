83527fdc9eb3c92f3da1d6bfddd0c9ee9dc99ffc
Nandinikona11 <101653632+Nandinikona11@users.noreply.github.com>
2022-03-31 10:16:11 +0530

    Add files via upload

---
+++ 3_Implementation/src/add.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <time.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include <string.h>
+#include"namevalid.h"
+#define MAX_YR  9999
+#define MIN_YR  1900
+#define MAX_SIZE_USER_NAME 30
+#define MAX_SIZE_PASSWORD  20
+#define FILE_NAME  "AticleworldLibBookS.bin"
+// Macro related to the books info
+#define MAX_BOOK_NAME   50
+#define MAX_AUTHOR_NAME 50
+#define MAX_STUDENT_NAME 50
+#define MAX_STUDENT_ADDRESS 300
+#define FILE_HEADER_SIZE  sizeof(sFileHeader)
+//structure to store date
+typedef struct
+{
+    int yyyy;
+    int mm;
+    int dd;
+} Date;
+typedef struct
+{
+    char username[MAX_SIZE_USER_NAME];
+    char password[MAX_SIZE_PASSWORD];
+} sFileHeader;
+typedef struct// to call in program
+{
+    unsigned int books_id; // declare the integer data type
+    char bookName[MAX_BOOK_NAME];// declare the character data type
+    char authorName[MAX_AUTHOR_NAME];// declare the charecter data type
+    char studentName[MAX_STUDENT_NAME];// declare the character data type
+    char studentAddr[MAX_STUDENT_ADDRESS];// declare the character data type
+    Date bookIssueDate;// declare the integer data type
+} s_BooksInfo;
+void addBook()
+{
+    int days;
+    s_BooksInfo addBook = {0};
+    FILE *fp = NULL;
+    int status = 0;
+    fp = fopen(FILE_NAME,"ab+");
+    if(fp == NULL)
+    {
+        printf("File is not opened\n");
+        exit(1);
+    }
+    printf("\n\n\t\t\tENTER YOUR DETAILS BELOW:");
+    printf("\n\t\t\t---------------------------------------------------------------------------\n");
+    printf("\n\t\t\tBook ID NO  = ");
+    fflush(stdin);
+    scanf("%u",&addBook.books_id);
+    do
+    {
+        printf("\n\t\t\tBook Name  = ");
+        fflush(stdin);
+        fgets(addBook.bookName,MAX_BOOK_NAME,stdin);
+        status = isNameValid(addBook.bookName);
+        if (!status)
+        {
+            printf("\n\t\t\tName contain invalid character. Please enter again.");
+        }
+    }
+    while(!status);
+    do
+    {
+        printf("\n\t\t\tAuthor Name  = ");
+        fflush(stdin);
+        fgets(addBook.authorName,MAX_AUTHOR_NAME,stdin);
+        status =isNameValid( addBook.authorName);
+        if (!status)
+        {
+            printf("\n\t\t\tName contain invalid character. Please enter again.");
+        }
+    }
+    while(!status);
+    do
+    {
+        printf("\n\t\t\tStudent Name  = ");
+        fflush(stdin);
+        fgets(addBook.studentName,MAX_STUDENT_NAME,stdin);
+        status =isNameValid( addBook.studentName);
+        if (!status)
+        {
+            printf("\n\t\t\tName contain invalid character. Please enter again.");
+        }
+    }
+    while(!status);
+    fwrite(&addBook,sizeof(addBook), 1, fp);
+    fclose(fp);
+}
