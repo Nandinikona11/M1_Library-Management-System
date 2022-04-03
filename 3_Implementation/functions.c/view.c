96651f5ad7a9e07a92a3e2c30aba303e6529e6b4
Nandinikona11 <101653632+Nandinikona11@users.noreply.github.com>
2022-04-03 14:53:47 +0530

    Create view.c

---
+++ 3_Implementation/functions.c/view.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <time.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include <string.h>
+#define MAX_YR  9999
+#define MIN_YR  1900
+#define MAX_SIZE_USER_NAME 30
+#define MAX_SIZE_PASSWORD  20
+#define FILE_NAME  "abc.c"
+// Macro related to the books info
+#define MAX_BOOK_NAME   50
+#define MAX_AUTHOR_NAME 50
+#define MAX_STUDENT_NAME 50
+#define MAX_STUDENT_ADDRESS 300
+#define FILE_HEADER_SIZE  sizeof(sFileHeader)
+//structure to store date
+typedef struct
+{
+    int yyyy;
+    int mm;
+    int dd;
+} Date;
+typedef struct
+{
+    char username[MAX_SIZE_USER_NAME];
+    char password[MAX_SIZE_PASSWORD];
+} sFileHeader;
+typedef struct// to call in program
+{
+    unsigned int books_id; // declare the integer data type
+    char bookName[MAX_BOOK_NAME];// declare the character data type
+    char authorName[MAX_AUTHOR_NAME];// declare the charecter data type
+    char studentName[MAX_STUDENT_NAME];// declare the character data type
+    char studentAddr[MAX_STUDENT_ADDRESS];// declare the character data type
+    Date bookIssueDate;// declare the integer data type
+} s_BooksInfo;
+void viewBooks()
+{
+    int found = 0;
+    char bookName[MAX_BOOK_NAME] = {0};
+    s_BooksInfo addBook = {0};
+    FILE *fp = NULL;
+    int status = 0;
+    unsigned int countBook = 1;
+    fp = fopen(FILE_NAME,"rb");
+    if(fp == NULL)
+    {
+        printf("File is not opened\n");
+        exit(1);
+    }
+    if (fseek(fp,FILE_HEADER_SIZE,SEEK_SET) != 0)
+    {
+        fclose(fp);
+        printf("Facing issue while reading file\n");
+        exit(1);
+    }
+    while (fread (&addBook, sizeof(addBook), 1, fp))
+    {
+        printf("\n\t\t\tBook Count = %d\n\n",countBook);
+        printf("\t\t\tBook id = %u",addBook.books_id);
+        printf("\n\t\t\tBook name = %s",addBook.bookName);
+        printf("\t\t\tBook authorName = %s",addBook.authorName);
+        printf("\n book is added");
+        found = 1;
+        ++countBook;
+    }
+    fclose(fp);
+    if(!found)
+    {
+        printf("\n\t\t\tNo Record");
+    }
+    printf("\n\n\t\t\tPress any key to go to main menu.....");
+    fflush(stdin);
+    getchar();
+}
