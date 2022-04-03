5e96b73eff08a0d17b8f31541a59ae5850bf050c
Nandinikona11 <101653632+Nandinikona11@users.noreply.github.com>
2022-04-03 14:51:43 +0530

    Create search.c

---
+++ 3_Implementation/functions.c/search.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <time.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include <string.h>
+#include"add.h"
+#define MAX_YR  9999
+#define MIN_YR  1900
+#define MAX_SIZE_USER_NAME 30
+#define MAX_SIZE_PASSWORD  20
+#define FILE_NAME  "abc.c"
+// Macro related to the books info
+#define MAX_BOOK_NAME   50
+#define MAX_AUTHOR_NAME 50
+#define MAX_STUDENT_NAME 50
+#define MAX_STUDENT_ADDRESS 300
+#define FILE_HEADER_SIZE  sizeof(sFileHeader)
+//structure to store date
+typedef struct
+{
+    int yyyy;
+    int mm;
+    int dd;
+} Date;
+typedef struct
+{
+    char username[MAX_SIZE_USER_NAME];
+    char password[MAX_SIZE_PASSWORD];
+} sFileHeader;
+typedef struct// to call in program
+{
+    unsigned int books_id; // declare the integer data type
+    char bookName[MAX_BOOK_NAME];// declare the character data type
+    char authorName[MAX_AUTHOR_NAME];// declare the charecter data type
+    char studentName[MAX_STUDENT_NAME];// declare the character data type
+    char studentAddr[MAX_STUDENT_ADDRESS];// declare the character data type
+    Date bookIssueDate;// declare the integer data type
+} s_BooksInfo;
+void searchBooks()
+{
+    int found = 0;
+    char bookName[MAX_BOOK_NAME] = {0};
+    s_BooksInfo addBook = {0};
+    FILE *fp = NULL;
+    int status = 0;
+    fp = fopen(FILE_NAME,"rb");
+    if(fp == NULL)
+    {
+        printf("\n\t\t\tFile is not opened\n");
+        exit(1);
+    }
+    //put the control on books detail
+    if (fseek(fp,FILE_HEADER_SIZE,SEEK_SET) != 0)
+    {
+        fclose(fp);
+        printf("\n\t\t\tFacing issue while reading file\n");
+        exit(1);
+    }
+    printf("\n\n\t\t\tEnter Book Name to search:");
+    fflush(stdin);
+    fgets(bookName,MAX_BOOK_NAME,stdin);
+    while (fread (&addBook, sizeof(addBook), 1, fp))
+    {
+        if(!strcmp(addBook.bookName, bookName))
+        {
+            found = 1;
+            break;
+        }
+    }
+    if(found)
+    {
+        printf("\n\t\t\tBook id = %u\n",addBook.books_id);
+        printf("\t\t\tBook name = %s",addBook.bookName);
+        printf("\t\t\tBook authorName = %s",addBook.authorName);
+        printf("\t\t\tBook issue date(day/month/year) =  (%d/%d/%d)",addBook.bookIssueDate.dd,
+               addBook.bookIssueDate.mm, addBook.bookIssueDate.yyyy);
+    }
+    else
+    {
+        printf("\n\t\t\tNo Record");
+    }
+    fclose(fp);
+    printf("\n\n\n\t\t\tPress any key to go to main menu.....");
+    getchar();
+}
