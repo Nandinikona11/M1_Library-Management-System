76b1a783b76cd828b353e8079028018123e6a10b
Nandinikona11 <101653632+Nandinikona11@users.noreply.github.com>
2022-04-03 10:31:08 +0530

    Create library_operations.c

---
+++ 3_Implementation/src/library_operations.c
@@ -0,0 +1,81 @@
+/**
+ * @file library_operations.c
+ * @author Nandinikona(nandinikona123@gmail.com)
+ * @brief This is the source code which contains all the functions for the project
+ * @version 0.1
+ * @date 2022-04-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <library_operations.h>
+
+/**
+ * @brief prints the input name
+ * @return Text file generated with user input name
+ */
+void addbook()
+{
+    return("project.txt","ab+")
+}
+
+/**
+ * @brief the name of the person you want to see the detail
+ * @return text file of the searched name
+ */
+void searchfun()
+{
+    return ("project.txt","r");
+}
+
+/**
+ * @brief lists the phonebook data
+ * @return text file of data stored
+*/
+
+void listfun()
+{
+    return("project.txt","r");
+}
+
+/**
+ * @brief modify the input data
+ * @return the modified valuew
+*/
+void modifyfun()
+{
+     return ("project.txt","r");
+}
+
+/**
+ * @brief delete the phonebook data
+ */
+    void deletefun()
+    {
+
+    }
+
+    /**
+ * @brief retruns to main page
+ 
+ */
+    void exitfun()
+    {
+
+    }
+
+    /**
+ * @brief verifying password from the user
+ * 
+ */
+    void password(void)
+    {
+
+    }
+    /**
+ * @brief show menu options 
+ */
+    void menu()
+    {
+
+    }
