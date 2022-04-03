74e7dd2286d244e513a64680d5415eacbdc57385
Nandinikona11 <101653632+Nandinikona11@users.noreply.github.com>
2022-04-03 14:49:49 +0530

    Create delete.c

---
+++ 3_Implementation/functions.c/delete.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <time.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include <string.h>
+#define MAX_YR  9999
+#define MIN_YR  1900
+#define MAX_SIZE_USER_NAME 30
+#define MAX_SIZE_PASSWORD  20
+#define FILE_NAME  "abc.c"
+// Macro related to the books info
+#define MAX_BOOK_NAME   50
+#define MAX_AUTHOR_NAME 50
+#define MAX_STUDENT_NAME 50
+#define MAX_STUDENT_ADDRESS 300
+#define FILE_HEADER_SIZE  sizeof(sFileHeader)
+//structure to store date
+typedef struct
+{
+    int yyyy;
+    int mm;
+    int dd;
+} Date;
+typedef struct
+{
+    char username[MAX_SIZE_USER_NAME];
+    char password[MAX_SIZE_PASSWORD];
+} sFileHeader;
+typedef struct// to call in program
+{
+    unsigned int books_id; // declare the integer data type
+    char bookName[MAX_BOOK_NAME];// declare the character data type
+    char authorName[MAX_AUTHOR_NAME];// declare the charecter data type
+    char studentName[MAX_STUDENT_NAME];// declare the character data type
+    char studentAddr[MAX_STUDENT_ADDRESS];// declare the character data type
+    Date bookIssueDate;// declare the integer data type
+} s_BooksInfo;
+void deleteBooks()
+{
+    int found = 0;
+    int bookDelete = 0;
+    sFileHeader fileHeaderInfo = {0};
+    char bookName[MAX_BOOK_NAME] = {0};
+    s_BooksInfo addBook = {0};
+    FILE *fp = NULL;
+    FILE *tmpFp = NULL;
+    int status = 0;
+    fp = fopen(FILE_NAME,"rb");
+    if(fp == NULL)
+    {
+        printf("File is not opened\n");
+        exit(1);
+    }
+    tmpFp = fopen("tmp.bin","wb");
+    if(tmpFp == NULL)
+    {
+        fclose(fp);
+        printf("File is not opened\n");
+        exit(1);
+    }
+    fread (&fileHeaderInfo,FILE_HEADER_SIZE, 1, fp);
+    fwrite(&fileHeaderInfo,FILE_HEADER_SIZE, 1, tmpFp);
+    printf("\n\t\t\tEnter Book ID NO. for delete:");
+    scanf("%d",&bookDelete);
+    while (fread (&addBook, sizeof(addBook), 1, fp))
+    {
+        if(addBook.books_id != bookDelete)
+        {
+            fwrite(&addBook,sizeof(addBook), 1, tmpFp);
+        }
+        else
+        {
+            found = 1;
+        }
+    }
+    (found)? printf("\n\t\t\tRecord deleted successfully....."):printf("\n\t\t\tRecord not found");
+    fclose(fp);
+    fclose(tmpFp);
+    remove(FILE_NAME);
+    rename("tmp.bin",FILE_NAME);
+}
